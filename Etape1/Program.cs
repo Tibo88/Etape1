3dafbf4c725b5ae589d662fbd9065c3a267083c8
Tibo88 <thibault.sevin@outlook.com>
2025-03-01 14:11:09 +0100

    Update Program.cs

--- Etape1/Program.cs
+++ Etape1/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 
+
 class Program
 {
     static void Main()
@@ -13,22 +14,45 @@ class Program
 
         Console.WriteLine("Liste d'adjacence:");
         graphe.CreerListeAdjacence();
-        graphe.AfficherListeAdjacence();
+        graphe.AfficherListe();
 
         Console.WriteLine("\nMatrice d'adjacence:");
         graphe.CreerMatriceAdjacence();
-        graphe.AfficherMatriceAdjacence();
+        graphe.AfficherMatrice();
 
-        // Parcours en profondeur et largeur
+        //Tests parcours profondeur et largeur
         Console.WriteLine("\nParcours en profondeur depuis le nœud 1:");
-        graphe.DFS(1);
+        foreach (var noeud in graphe.DFS(1))
+        {
+            Console.Write(noeud + ", ");
+        }
 
         Console.WriteLine("\nParcours en largeur depuis le nœud 1:");
-        graphe.BFS(1);
+        foreach (var noeud in graphe.BFS(1))
+        {
+            Console.Write(noeud + ", ");
+        }
 
-        // Générer une image du graphe
-        Console.WriteLine("\nGénération de l'image du graphe...");
+        //Afficher le graphe
+        string imagePath = "graph.png";
+        graphe.GenererGraphe(imagePath);
+        Console.WriteLine($"Image du graphe générée : {imagePath}");
+        Process.Start(new ProcessStartInfo(imagePath) { UseShellExecute = true });
+
+        //on vérifie si le graphe est connexe
+        if (graphe.TestConnexe())
+        {
+            Console.WriteLine("Le graphe est connexe");
+        }
+        else
+        {
+            Console.WriteLine("Le graphe n'est pas connexe");
+        }
+
+
+
+
     }
 
-    
+
 }

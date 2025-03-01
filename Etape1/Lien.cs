f3e9e60d093b5b86a4da8ee6aef9843894e86b90
Tibo88 <thibault.sevin@outlook.com>
2025-03-01 17:28:00 +0100

    Update Lien.cs

--- Etape1/Lien.cs
+++ Etape1/Lien.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Drawing;
-using System.Drawing.Imaging;
+
 
 namespace Etape1
 {
@@ -26,10 +25,6 @@ namespace Etape1
 
         public Noeud<T> Destination
         {
-            get { return destination;  }
-        }
-    }
-}
             get { return destination;  }
         }
     }

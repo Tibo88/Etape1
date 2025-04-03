c152b459373312f9a851f900059cdbd52a8a4a64
Eliott748 <eliottsacrez@gmail.com>
2025-04-03 21:49:03 +0200

    Update Program.cs

--- Etape1/Program.cs
+++ Etape1/Program.cs
@@ -8,17 +8,29 @@ using System.IO;
 class Program
 {
     static void Main()
-    {
-        Graph<int> graphe = new Graph<int>();
-        graphe.ChargerDepuisFichier("Noeuds2.txt", "Arcs2.txt");
-        graphe.AjouterLiensManquants();
+{
+    Graph<int> graphe = new Graph<int>();
+    graphe.ChargerDepuisFichier("Noeuds2.txt", "Arcs2.txt");
+    graphe.AjouterLiensManquants();
 
-        Console.WriteLine("Liste d'adjacence:");
-        graphe.CreerListeAdjacence();
-        graphe.AfficherListe();
-        List<int> chemin = graphe.Dijkstra(1, 115);
-        graphe.AfficherChemin(chemin);
-        
-        
-    }
+    Console.WriteLine("Liste d'adjacence:");
+    graphe.CreerListeAdjacence();
+    graphe.AfficherListe();
+    Console.WriteLine();
+    Console.WriteLine("Exécution de Floyd-Warshall");
+    graphe.FloydWarshall();
+    graphe.AfficherCheminPlusCourt(80, 148);
+    Console.WriteLine();
+
+    Console.WriteLine("Exécution de Dijkstra");
+    List<int> cheminDijkstra = graphe.Dijkstra(80, 148);
+    graphe.AfficherChemin(cheminDijkstra);
+    Console.WriteLine();
+
+    Console.WriteLine("Exécution de Bellman-Ford");
+    List<int> cheminBellmanFord = graphe.BellmanFord(80, 148);
+    graphe.AfficherChemin(cheminBellmanFord);
+
+    // MenuPrincipal();
+}
 }

40b60e9b56296c7f92535df764fa19ccb5d9720b
Eliott748 <eliottsacrez@gmail.com>
2025-05-05 10:09:00 +0200

    Update Projet_rendu_1.sql

--- Projet_rendu_1.sql
+++ Projet_rendu_1.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE AppliV2;
-USE AppliV2;
+CREATE DATABASE AppliV3;
+USE AppliV3;
 
 CREATE TABLE client_particulier (
     numero INT PRIMARY KEY,
@@ -8,7 +8,7 @@ CREATE TABLE client_particulier (
     adresse TEXT,
     telephone VARCHAR(20),
     email VARCHAR(100),
-    metro_proche VARCHAR(50),
+    metro_proche INT,
     identifiant VARCHAR(50) UNIQUE,
     mot_de_passe VARCHAR(255)
 );
@@ -19,7 +19,7 @@ CREATE TABLE client_entreprise (
     adresse TEXT,
     telephone VARCHAR(20),
     nom_referent VARCHAR(50),
-    metro_proche VARCHAR(50),
+    metro_proche INT,
     email VARCHAR(100),
     identifiant VARCHAR(50) UNIQUE,
     mot_de_passe VARCHAR(255)
@@ -32,13 +32,18 @@ CREATE TABLE cuisinier (
     adresse TEXT,
     telephone VARCHAR(20),
     email VARCHAR(100),
-    metro_proche VARCHAR(50),
+    metro_proche INT,
     identifiant VARCHAR(50) UNIQUE,
     mot_de_passe VARCHAR(255)
 );
 
 CREATE TABLE commande (
-    numero_commande INT PRIMARY KEY
+    numero_commande INT PRIMARY KEY,
+    client_id VARCHAR(50),
+    cuisinier_id VARCHAR(50),
+    date_commande DATE,
+    FOREIGN KEY (client_id) REFERENCES client_particulier(identifiant),
+    FOREIGN KEY (cuisinier_id) REFERENCES cuisinier(identifiant)
 );
 
 CREATE TABLE sous_commandes (
@@ -46,7 +51,9 @@ CREATE TABLE sous_commandes (
     date_livraison DATE,
     adresse_livraison TEXT,
     numero_commande INT,
-    FOREIGN KEY (numero_commande) REFERENCES commande(numero_commande)
+    plat_id INT,
+    FOREIGN KEY (numero_commande) REFERENCES commande(numero_commande),
+    FOREIGN KEY (plat_id) REFERENCES plat(id_plat)
 );
 
 CREATE TABLE plat (
@@ -66,3 +73,26 @@ CREATE TABLE ingredients (
     ingredient VARCHAR(100),
     volume DECIMAL(10,2)
 );
+DESCRIBE commande;
+
+SELECT c.client_id, cp.metro_proche
+FROM commande c
+JOIN client_particulier cp ON cp.identifiant = c.client_id
+WHERE c.numero_commande = 1;
+SELECT identifiant FROM client_particulier;
+UPDATE commande
+SET client_id = 'Eliott'
+WHERE numero_commande = 1;
+
+SELECT metro_proche FROM client_particulier WHERE identifiant = 'Eliott';
+
+SELECT metro_proche FROM cuisinier WHERE identifiant = 'Tibo';
+
+SELECT numero_commande, client_id FROM commande WHERE numero_commande = 1;
+SELECT * FROM commande;
+SELECT * FROM sous_commandes;
+
+SELECT c.numero_commande, c.client_id, cp.metro_proche
+FROM commande c
+JOIN client_particulier cp ON cp.identifiant = c.client_id
+WHERE c.numero_commande IN (1004, 1005);

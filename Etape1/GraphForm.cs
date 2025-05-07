da3265df5309818f3b7cf00cf3977a2619fc54d2
Eliott748 <eliottsacrez@gmail.com>
2025-05-07 14:53:29 +0200

    Update GraphForm.cs

--- Etape1/GraphForm.cs
+++ Etape1/GraphForm.cs
@@ -1,138 +1,287 @@
-using SkiaSharp;
+
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+using System.Data;
+using System.Diagnostics;
+using MySql.Data.MySqlClient;
+using SkiaSharp;
 
 namespace Etape2
 {
-    public class GraphForm
+    public class GraphRelation
     {
-        /// <summary>
-        /// Génère un plan du métro à partir des données de graphes et l'enregistre dans un fichier image.
-        /// </summary>
-        /// <param name="graphe">Le graphe contenant les stations du métro et leurs liens.</param>
-        /// <param name="nomFichier">Le nom du fichier dans lequel l'image sera sauvegardée.</param>
-        /// <param name="chemin">La liste des stations représentant le chemin trouvé.</param>
-        public static void GenererPlanDuMetro(Graph<int> graphe, string nomFichier, List<int> chemin = null)
-        {
-            // Taille de l'image
-            int largeurImage = 1400;
-            int hauteurImage = 1200;
+        private Dictionary<string, Utilisateur> Utilisateurs { get; set; }
+        private Dictionary<string, List<string>> ListeAdjacence { get; set; }
+        private string connectionString = "server=localhost;database=AppliV3;user=root;password=&Mot2passe;";
 
-            // Créer une image vide
+        public GraphRelation()
+        {
+            Utilisateurs = new Dictionary<string, Utilisateur>();
+            ListeAdjacence = new Dictionary<string, List<string>>();
+            ChargerDonneesDepuisBase();
+            CreerListeAdjacence();
+        }
+
+        /// <summary>
+        /// Charge les données des utilisateurs depuis la base de données.
+        /// </summary>
+        private void ChargerDonneesDepuisBase()
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+
+                // Charger les clients particuliers
+                string query = "SELECT identifiant, nom, prenom FROM client_particulier";
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string identifiant;
+                        if (reader.IsDBNull(0))
+                            identifiant = "N/A";
+                        else
+                            identifiant = reader.GetString(0);
+
+                        string nom;
+                        if (reader.IsDBNull(1))
+                            nom = "N/A";
+                        else
+                            nom = reader.GetString(1);
+
+                        string prenom;
+                        if (reader.IsDBNull(2))
+                            prenom = "N/A";
+                        else
+                            prenom = reader.GetString(2);
+
+                        Utilisateurs[identifiant] = new Utilisateur(identifiant, nom, prenom, "client_particulier");
+
+                        //Console.WriteLine($"Client particulier chargé : {identifiant} - {nom} {prenom}");
+                    }
+                }
+
+                // Charger les clients entreprises
+                query = "SELECT identifiant, nom_entreprise, nom_referent FROM client_entreprise";
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string identifiant;
+                        if (reader.IsDBNull(0))
+                            identifiant = "N/A";
+                        else
+                            identifiant = reader.GetString(0);
+
+                        string nom;
+                        if (reader.IsDBNull(1))
+                            nom = "N/A";
+                        else
+                            nom = reader.GetString(1);
+
+                        string prenom;
+                        if (reader.IsDBNull(2))
+                            prenom = "N/A";
+                        else
+                            prenom = reader.GetString(2);
+
+                        Utilisateurs[identifiant] = new Utilisateur(identifiant, nom, prenom, "client_entreprise");
+
+                        // Console.WriteLine($"Client entreprise chargé : {identifiant} - {nom} {prenom}");
+                    }
+                }
+
+                // Charger les cuisiniers
+                query = "SELECT identifiant, nom, prenom FROM cuisinier";
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string identifiant;
+                        if (reader.IsDBNull(0))
+                            identifiant = "N/A";
+                        else
+                            identifiant = reader.GetString(0);
+
+                        string nom;
+                        if (reader.IsDBNull(1))
+                            nom = "N/A";
+                        else
+                            nom = reader.GetString(1);
+
+                        string prenom;
+                        if (reader.IsDBNull(2))
+                            prenom = "N/A";
+                        else
+                            prenom = reader.GetString(2);
+
+                        Utilisateurs[identifiant] = new Utilisateur(identifiant, nom, prenom, "cuisinier");
+
+                        // Console.WriteLine($"Cuisinier chargé : {identifiant} - {nom} {prenom}");
+                    }
+                }
+
+                // Charger les commandes
+                query = "SELECT client_id, cuisinier_id FROM commande";
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string clientId;
+                        if (reader.IsDBNull(0))
+                            clientId = "N/A";
+                        else
+                            clientId = reader.GetString(0);
+
+                        string cuisinierId;
+                        if (reader.IsDBNull(1))
+                            cuisinierId = "N/A";
+                        else
+                            cuisinierId = reader.GetString(1);
+
+                        AjouterLien(clientId, cuisinierId);
+
+                        //Console.WriteLine($"Commande chargée : Client {clientId} -> Cuisinier {cuisinierId}");
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Crée la liste d'adjacence à partir des liens entre les utilisateurs.
+        /// </summary>
+        private void CreerListeAdjacence()
+        {
+            ListeAdjacence.Clear();
+            foreach (var utilisateur in Utilisateurs.Values)
+            {
+                ListeAdjacence[utilisateur.Identifiant] = new List<string>();
+            }
+
+            foreach (var utilisateur in Utilisateurs.Values)
+            {
+                foreach (var lien in utilisateur.Liens)
+                {
+                    ListeAdjacence[utilisateur.Identifiant].Add(lien);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Ajoute un lien entre un client et un cuisinier.
+        /// </summary>
+        /// <param name="clientId">L'ID du client.</param>
+        /// <param name="cuisinierId">L'ID du cuisinier.</param>
+        public void AjouterLien(string clientId, string cuisinierId)
+        {
+            if (Utilisateurs.ContainsKey(clientId) && Utilisateurs.ContainsKey(cuisinierId))
+            {
+                Utilisateurs[clientId].Liens.Add(cuisinierId);
+                Utilisateurs[cuisinierId].Liens.Add(clientId);
+                CreerListeAdjacence();
+            }
+        }
+
+        /// <summary>
+        /// Affiche la liste d'adjacence des utilisateurs.
+        /// </summary>
+        public void AfficherListeAdjacence()
+        {
+            //Console.WriteLine("Liste d'adjacence :");
+            foreach (var utilisateur in ListeAdjacence)
+            {
+               // Console.Write($"{utilisateur.Key} -> ");
+               // Console.WriteLine(string.Join(", ", utilisateur.Value));
+            }
+        }
+
+        /// <summary>
+        /// Génère et affiche le graphe des relations entre les utilisateurs.
+        /// </summary>
+        /// <param name="nomFichier">Le nom du fichier où sauvegarder le graphe.</param>
+        public void GenererGraphe(string nomFichier)
+        {
+            int largeurImage = 1000;
+            int hauteurImage = 800;
+
             using (var surface = SKSurface.Create(new SKImageInfo(largeurImage, hauteurImage)))
             {
                 var canvas = surface.Canvas;
-                canvas.Clear(SKColors.White); // Fond blanc
+                canvas.Clear(SKColors.White);
 
-                // Calculer l'échelle pour convertir les coordonnées de la station en pixels
-                double minLongitude = double.MaxValue;
-                double maxLongitude = double.MinValue;
-                double minLatitude = double.MaxValue;
-                double maxLatitude = double.MinValue;
+                // Calculer les positions des noeuds (pour simplifier, on les place en cercle)
+                double angle = 0;
+                double angleIncrement = 2 * Math.PI / Utilisateurs.Count;
+                Dictionary<string, (int x, int y)> positions = new Dictionary<string, (int x, int y)>();
 
-                foreach (var noeud in graphe.Noeuds.Values)
+                foreach (var utilisateur in Utilisateurs.Values)
                 {
-                    if (noeud.Longitude < minLongitude)
-                        minLongitude = noeud.Longitude;
-                    if (noeud.Longitude > maxLongitude)
-                        maxLongitude = noeud.Longitude;
-                    if (noeud.Latitude < minLatitude)
-                        minLatitude = noeud.Latitude;
-                    if (noeud.Latitude > maxLatitude)
-                        maxLatitude = noeud.Latitude;
+                    int x = (int)(largeurImage / 2 + 300 * Math.Cos(angle));
+                    int y = (int)(hauteurImage / 2 + 300 * Math.Sin(angle));
+                    positions[utilisateur.Identifiant] = (x, y);
+                    angle += angleIncrement;
                 }
 
-
-                double echelleLongitude = largeurImage / (maxLongitude - minLongitude);
-                double echelleLatitude = hauteurImage / (maxLatitude - minLatitude);
-
-                // Dessiner les liens entre les stations
-                foreach (var noeud in graphe.Noeuds.Values)
+                // Dessiner les liens
+                foreach (var utilisateur in Utilisateurs.Values)
                 {
-                    // Inverser la latitude pour que les stations plus au nord apparaissent plus bas
-                    int x1 = (int)((noeud.Longitude - minLongitude) * echelleLongitude);
-                    int y1 = (int)((maxLatitude - noeud.Latitude) * echelleLatitude);  // Inversion ici
-
-                    // Dessiner les voisins
-                    foreach (var lien in noeud.Liens)
+                    foreach (var lien in utilisateur.Liens)
                     {
-                        Noeud<int> voisin = lien.Destination;
-                        int x2 = (int)((voisin.Longitude - minLongitude) * echelleLongitude);
-                        int y2 = (int)((maxLatitude - voisin.Latitude) * echelleLatitude);  // Inversion ici
-
-                        // Dessiner une ligne entre les stations
-                        var paint = new SKPaint
-                        {
-                            Color = SKColors.Black,
-                            StrokeWidth = 2,
-                            IsAntialias = true
-                        };
-
-                        canvas.DrawLine(x1, y1, x2, y2, paint);
+                        var pos1 = positions[utilisateur.Identifiant];
+                        var pos2 = positions[lien];
+                        canvas.DrawLine(pos1.x, pos1.y, pos2.x, pos2.y, new SKPaint { Color = SKColors.Black, StrokeWidth = 2 });
                     }
                 }
 
-                // Dessiner les stations
-                foreach (var noeud in graphe.Noeuds.Values)
+                // Dessiner les noeuds
+                foreach (var utilisateur in Utilisateurs.Values)
                 {
-                    // Inverser la latitude pour que les stations plus au nord apparaissent plus bas
-                    int x = (int)((noeud.Longitude - minLongitude) * echelleLongitude);
-                    int y = (int)((maxLatitude - noeud.Latitude) * echelleLatitude);  // Inversion ici
-
-                    // Dessiner un cercle représentant la station
-                    var paint = new SKPaint
-                    {
-                        Color = SKColors.Red,
-                        IsAntialias = true
-                    };
-                    canvas.DrawCircle(x, y, 5, paint);
-
-                    // Dessiner le nom de la station à côté
-                    var textPaint = new SKPaint
-                    {
-                        Color = SKColors.Black,
-                        TextSize = 14,
-                        IsAntialias = true
-                    };
-
-                    canvas.DrawText(noeud.Nom, x + 5, y - 5, textPaint);
+                    var pos = positions[utilisateur.Identifiant];
+                    canvas.DrawCircle(pos.x, pos.y, 10, new SKPaint { Color = SKColors.Red });
+                    canvas.DrawText(utilisateur.Identifiant, pos.x + 15, pos.y - 5, new SKPaint { Color = SKColors.Black, TextSize = 12 });
                 }
 
-                // Dessiner le chemin trouvé
-                if (chemin != null && chemin.Count > 1)
-                {
-                    var cheminPaint = new SKPaint
-                    {
-                        Color = SKColors.Blue,
-                        StrokeWidth = 4,
-                        IsAntialias = true
-                    };
-
-                    for (int i = 0; i < chemin.Count - 1; i++)
-                    {
-                        var noeud1 = graphe.Noeuds[chemin[i]];
-                        var noeud2 = graphe.Noeuds[chemin[i + 1]];
-
-                        int x1 = (int)((noeud1.Longitude - minLongitude) * echelleLongitude);
-                        int y1 = (int)((maxLatitude - noeud1.Latitude) * echelleLatitude);
-                        int x2 = (int)((noeud2.Longitude - minLongitude) * echelleLongitude);
-                        int y2 = (int)((maxLatitude - noeud2.Latitude) * echelleLatitude);
-
-                        canvas.DrawLine(x1, y1, x2, y2, cheminPaint);
-                    }
-                }
-
-                // Sauvegarder l'image dans un fichier
+                // Sauvegarder l'image
                 using (var image = surface.Snapshot())
                 using (var data = image.Encode())
                 using (var stream = File.OpenWrite(nomFichier))
                 {
                     data.SaveTo(stream);
-                    Console.WriteLine("Le plan du métro a été sauvegardé dans : " + nomFichier);
+                    //Console.WriteLine("Le graphe a été sauvegardé dans : " + nomFichier);
+                }
+                
+
+                try
+                {
+                    Process.Start(new ProcessStartInfo(nomFichier) { UseShellExecute = true });
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Erreur lors de l'ouverture du fichier : {ex.Message}");
                 }
             }
+        }
+    }
+
+    public class Utilisateur
+    {
+        public string Identifiant { get; }
+        public string Nom { get; }
+        public string Prenom { get; }
+        public string Type { get; }
+        public List<string> Liens { get; }
+
+        public Utilisateur(string identifiant, string nom, string prenom, string type)
+        {
+            Identifiant = identifiant;
+            Nom = nom;
+            Prenom = prenom;
+            Type = type;
+            Liens = new List<string>();
         }
     }
 }

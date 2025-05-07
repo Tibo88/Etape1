d2303876f8cc399656328c1c0e65433061edece7
Eliott748 <eliottsacrez@gmail.com>
2025-05-07 16:00:05 +0200

    Update GraphRelation.cs

--- Etape1/GraphRelation.cs
+++ Etape1/GraphRelation.cs
@@ -23,7 +23,7 @@ namespace Etape2
         }
 
         /// <summary>
-        /// Charge les données des utilisateurs depuis la base de données.
+        /// Charge les données des utilisateurs depuis la base de données
         /// </summary>
         private void ChargerDonneesDepuisBase()
         {
@@ -152,7 +152,7 @@ namespace Etape2
         }
 
         /// <summary>
-        /// Crée la liste d'adjacence à partir des liens entre les utilisateurs.
+        /// Crée la liste d'adjacence à partir des liens entre les utilisateurs
         /// </summary>
         private void CreerListeAdjacence()
         {
@@ -172,10 +172,10 @@ namespace Etape2
         }
 
         /// <summary>
-        /// Ajoute un lien entre un client et un cuisinier.
+        /// Ajoute un lien entre un client et un cuisinier
         /// </summary>
-        /// <param name="clientId">L'ID du client.</param>
-        /// <param name="cuisinierId">L'ID du cuisinier.</param>
+        /// <param name="clientId">L'ID du client</param>
+        /// <param name="cuisinierId">L'ID du cuisinier</param>
         public void AjouterLien(string clientId, string cuisinierId)
         {
             if (Utilisateurs.ContainsKey(clientId) && Utilisateurs.ContainsKey(cuisinierId))
@@ -187,7 +187,7 @@ namespace Etape2
         }
 
         /// <summary>
-        /// Affiche la liste d'adjacence des utilisateurs.
+        /// Affiche la liste d'adjacence des utilisateurs
         /// </summary>
         public void AfficherListeAdjacence()
         {
@@ -200,9 +200,9 @@ namespace Etape2
         }
 
         /// <summary>
-        /// Génère et affiche le graphe des relations entre les utilisateurs.
+        /// Génère et affiche le graphe des relations entre les utilisateurs
         /// </summary>
-        /// <param name="nomFichier">Le nom du fichier où sauvegarder le graphe.</param>
+        /// <param name="nomFichier">Le nom du fichier où sauvegarder le graphe</param>
         public void GenererGraphe(string nomFichier)
         {
             int largeurImage = 1000;
